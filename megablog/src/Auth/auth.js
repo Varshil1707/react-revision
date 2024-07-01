9b5200bb1b426e884df011b2edcaa307d211d336
varshil shah <varshil.shah@ecosmob.com>
2024-07-01 17:17:56 +0530

    megaProject

---
+++ megablog/src/Auth/auth.js
@@ -0,0 +1,60 @@
+import { conf } from "../conf/config";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor() {
+        this.client
+            .setEndpoint(conf.appWriteUrl)
+            .setProject(conf.projectId);
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, name);
+            if (userAccount) {
+                // return userAccount
+                // call another method
+                return this.login(email, password)
+            } else {
+                return userAccount
+            }
+
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser() {
+    try {
+        await this.account.get()
+    } catch (err)
+     { throw err }
+
+     return null
+
+}
+    async logout() {
+    try {
+        await this.account.deleteSessions()
+    } catch (err)
+     { throw err }
+
+     return null
+}
+}
+
+const AuthService = new AuthService()
+
+export default AuthService
---
+++ megablog/src/conf/config.js
@@ -0,0 +1,7 @@
+export const conf = {
+    appWriteUrl: String('https://cloud.appwrite.io/v1'),
+    projectId: String('66825b71001f150d6a14'),
+    databaseId: String('668260320013294c222f'),
+    collectionId: String('6682604a0027c6919907'),
+    bucketId: String('6682798a003e203e8aeb')
+}
